1b1a7c07e40dc50c9c116ed1c59d6e865316238b
Huiping Li <Huiping.Li@cchmc.org>
2019-04-24 14:26:58 -0400

    Get latest login interface change and model change

---
+++ PHO-WebApp/PHO-WebApp/Models/UserDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace PHO_WebApp.Models
+{
+    public class UserDetails
+    {
+        public int PersonId { get; set; } //PersonId in [PHO].[reg].[Login], similar to user id
+
+        [Required(ErrorMessage = "User name is required.")]
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        public string LoginError { get; set; }
+    }
+}
