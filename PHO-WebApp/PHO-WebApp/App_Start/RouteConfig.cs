68d05e188196a1d3cd3cfab075bb20b1edf939e7
Huiping Li <Huiping.Li@cchmc.org>
2019-04-26 08:27:34 -0400

    Added Live Search to the Data Dictionary page before deploying to develop

--- PHO-WebApp/PHO-WebApp/App_Start/RouteConfig.cs
+++ PHO-WebApp/PHO-WebApp/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace PHO_WebApp
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+           
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
@@ -20,7 +20,7 @@ namespace PHO_WebApp
             );
 
             routes.MapRoute(
-               name: "",
+               name: "Patients",
                url: "{controller}/{action}/{id}",
                defaults: new { controller = "Patient", action = "GetPatients", id = UrlParameter.Optional }
            );
--- PHO-WebApp/PHO-WebApp/Controllers/LoginController.cs
+++ PHO-WebApp/PHO-WebApp/Controllers/LoginController.cs
@@ -1,16 +1,39 @@
-﻿using System;
+﻿using PHO_WebApp.DataAccessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
 
 namespace PHO_WebApp.Controllers
 {
     public class LoginController : Controller
     {
         // GET: Login
+
+        DataAccessLayer.LoginDAL userLogin = new LoginDAL();
+
         public ActionResult Login()
         {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection fc)
+        {
+            int id = userLogin.GetUserLogin(fc["UserName"], fc["Password"]);
+           
+            if (id > 0)
+            {
+                Session["username"] = fc["UserName"].ToString();
+                return RedirectToAction("GetPatients", "Patient");
+                //return RedirectToAction("DataDictionary", "Home");
+            }
+            else
+            {
+                ViewData["message"] = "Login failed!";
+            }
+      
             return View();
         }
     }
