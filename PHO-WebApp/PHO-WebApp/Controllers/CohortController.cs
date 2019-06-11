aa1e4f9d739870532c49b87b4d16c79b19535a2c
Hatten, Anthony <Anthony.Hatten@cchmc.org>
2019-06-11 14:30:34 -0400

    Additional tweaks to CohortBuilder v1

--- PHO-WebApp/PHO-WebApp/Controllers/CohortController.cs
+++ PHO-WebApp/PHO-WebApp/Controllers/CohortController.cs
@@ -14,11 +14,7 @@ namespace PHO_WebApp.Controllers
     public class CohortController : Controller
     {
         DataAccessLayer.CohortDAL cohortRecords = new CohortDAL();
-        //DataSet ds = cohortRecords.getAllActiveCohortRecords();
-        //ViewBag.Cohort = ds.Tables[0];
-        
-
-       
+     
             
         // GET: Cohort
         public ActionResult Index()
@@ -32,53 +28,9 @@ namespace PHO_WebApp.Controllers
             return View(CohortNew);
         }
 
-        //public ActionResult DisplayCohort()
-        //{
-        //    DataSet ds = cohortRecords.getAllActiveCohortRecords();
-        //    return View();
-        //    //string jsonresult = JsonConvert.SerializeObject(ds.Tables[0]);
-        //    ////foreach(var a in ds)
-        //    ////      {
-
-        //    ////  }
-        //    ////  ViewBag.Cohort = ds.Tables[0];
-        //    ////  return Json(new { Name = "bar", ShortName = "Blech",Description= "testDes", Owner ="est",ModifiedDate = "tes" }, JsonRequestBehavior.AllowGet);
-        //    ////  return View();
-        //    ////return Json(
-        //    ////   jsonresult
-        //    ////    , JsonRequestBehavior.AllowGet);
-        //    //return Json(JsonConvert.SerializeObject(new { item = ds.Tables[0] }), JsonRequestBehavior.AllowGet);
-        //    ////JsonRequestBehavior.AllowGet);
-
-
-        //    ////      { "data": "Id" },
-        //    ////{ "data": "Name" },
-        //    ////{ "data": "ShortName" },
-        //    ////{ "data": "Description"},
-        //    ////{ "data": "Owner"},
-        //    //{ "data": "ModifiedDate"}
-
-        //}
-
-        //public ActionResult DisplayCohort()
-        //{
-        //    //DataSet ds = cohortRecords.getAllActiveCohortRecords();
-        //    //ViewBag.Cohort = ds.Tables[0];
-
-        //    //Response.Redirect(Url.Action("DisplayCohort", "Cohort"));
-        //    //return View("DisplayCohort");
-
-        //    //return View("DataDictionary");
-
-        //}
         public PartialViewResult CohortDisplay()
         {
-            //DataSet ds = cohortRecords.getAllActiveCohortRecords();
-            //ViewBag.Cohort = ds.Tables[0];
-
-            ////Response.Redirect(Url.Action("DisplayCohort", "Cohort"));
             return PartialView("CohortDisplay");
-            ////return PartialView();
         }
     }
 
