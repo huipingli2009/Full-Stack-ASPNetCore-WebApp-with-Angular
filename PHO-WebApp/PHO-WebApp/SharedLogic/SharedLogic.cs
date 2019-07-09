d0791f4409e68c4da4e7ca618242b6c09a9cdc6f
Hatten, Anthony <Anthony.Hatten@cchmc.org>
2019-07-09 14:20:11 -0400

    Bug fix.

---
+++ PHO-WebApp/PHO-WebApp/SharedLogic/SharedLogic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PHO_WebApp.Models;
+
+namespace PHO_WebApp
+{
+    public static class SharedLogic
+    {
+        public static bool IsAuthenticated(StaffTypeEnum[] authorizedStaffTypes, object userDetails)
+        {
+            bool returnValue = false;
+
+            if (userDetails != null)
+            {
+                UserDetails user = (UserDetails)userDetails;
+                if (user.StaffTypeId > 0)
+                {
+                    foreach (StaffTypeEnum stafftype in authorizedStaffTypes)
+                    {
+                        if (user.StaffTypeId == (int)stafftype)
+                        {
+                            returnValue = true;
+                        }
+                    }
+                }
+            }
+
+            return returnValue;
+        }
+
+    }
+}
