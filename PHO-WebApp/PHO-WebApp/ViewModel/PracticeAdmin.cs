836216d660f24836a2d129e71111c649b796c85d
Huiping Li <Huiping.Li@cchmc.org>
2019-11-11 19:52:07 +0000

    Conflict resolutions for !5479

--- PHO-WebApp/PHO-WebApp/ViewModel/PracticeAdmin.cs
+++ PHO-WebApp/PHO-WebApp/ViewModel/PracticeAdmin.cs
@@ -90,7 +90,7 @@ namespace PHO_WebApp.ViewModel
             {
                 if (Mode == "AddStaff")
                 {
-                    vmPA.Insert(Entity);
+                    vmPA.Insert(Entity, UserLogin.PracticeId);
                     //Insert(vmPA);
                 }
                 else   //update staff
@@ -264,7 +264,7 @@ namespace PHO_WebApp.ViewModel
             PracStaff = std.getPracticeStaffs(UserLogin.PracticeId); 
         }
 
-        public void Insert(Staff model)
+        public void Insert(Staff model, int practiceId)
         {
             SqlCommand com = new SqlCommand("spAddNewStaff", con);
             com.CommandType = CommandType.StoredProcedure;
@@ -281,7 +281,7 @@ namespace PHO_WebApp.ViewModel
 
             //replace this part with coding when we have Create Staff part done
             //Create Staff is the prior step before creating user login per system design
-            com.Parameters["@PracticeId"].Value = UserLogin.PracticeId;           
+            com.Parameters["@PracticeId"].Value = practiceId; ;   //practice staff type id = 4           
 
             //hard code for practice staff. Will be replaced with Staff Type look ups
             com.Parameters["@StaffTypeId"].Value = 5;
@@ -420,7 +420,16 @@ namespace PHO_WebApp.ViewModel
                 com.Parameters["@Phone"].Value = DBNull.Value;
             }
 
-            com.Parameters["@NPI"].Value = model.NPI;
+            //com.Parameters["@NPI"].Value = model.NPI;
+
+            if (!String.IsNullOrWhiteSpace(model.NPI.ToString()))
+            {
+                com.Parameters["@NPI"].Value = model.NPI;
+            }
+            else
+            {
+                com.Parameters["@NPI"].Value = DBNull.Value;
+            }
 
             //com.Parameters["@CreatedById"].Value = model.NPI;
 
