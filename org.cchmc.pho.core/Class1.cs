e595cacd21cda238cbfe5268ab2a4234680facb5
Ray Stone <ray.stone@cchmc.org>
2020-02-03 13:39:14 -0500

    update to starter

--- org.cchmc.pho.core/Class1.cs
+++ org.cchmc.pho.core/Class1.cs
@@ -2,6 +2,12 @@
 
 namespace org.cchmc.pho.core
 {
+
+    //TODO : do we want to upgrade this to standard 2.1?
+    //https://devblogs.microsoft.com/dotnet/announcing-net-standard-2-1/
+
+
+
     public class Class1
     {
     }
