1091089bbe2d2d05b5deed32f7cceefebc5293f8
Uday Kumar Ambarapu <UdayKumar.Ambarapu@cchmc.org>
2020-03-11 15:12:30 -0400

    removed the unnecessary 'using' based on comments from sonarqube

--- org.cchmc.pho.api/Mappings/SearchResultsMappings.cs
+++ org.cchmc.pho.api/Mappings/SearchResultsMappings.cs
@@ -1,10 +1,7 @@
 ﻿using org.cchmc.pho.api.ViewModels;
 using org.cchmc.pho.core.Models;
 using AutoMapper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace org.cchmc.pho.api.Mappings
 {
